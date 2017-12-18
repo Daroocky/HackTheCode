c1d442df0a4e4500c70df452c95d85ea671cd8b2
Stefan Roock <s.roock@babiel.com>
2017-12-18 11:53:09 +0100

    Backspace eingefügt

--- dev/ts/Game.ts
+++ dev/ts/Game.ts
@@ -13,8 +13,8 @@ class Game {
     private codeGenerator: CodeGenerator;
 
     constructor() {
-        this.resultLog =  new ResultLog();
-        this.codeGenerator =  new CodeGenerator();
+        this.resultLog = new ResultLog();
+        this.codeGenerator = new CodeGenerator();
 
         this.registerEvents();
         this.createCode();
@@ -29,36 +29,42 @@ class Game {
             $("#lock").on("click", () => {
                 this.eventCheckCode()
             });
-            $("input").on("keyup", (e: any) => {
-                this.eventNavigate(e)
-                this.eventNextInput(e)
+            $("input, #lock").on("keyup", (e: any) => {
+                this.eventNavigate(e);
+                this.eventNextInput(e);
             });
         });
     }
 
     eventNavigate(event: any) {
-         if (event.keyCode == 37) {
+        if (event.keyCode == 37) {
             $(event.target).prev("input").focus().select();
         }
 
         if (event.keyCode == 39) {
             $(event.target).next("input").focus().select();
         }
+
+        if(event.keyCode == 8) {
+            if($(event.target).val() == "") {
+                $(event.target).prev("input").focus().val("");
+            }
+        }
     }
 
     eventNextInput(event: any) {
-        if(event.keyCode != 37 && event.keyCode != 39) {
+        if (event.keyCode != 37 && event.keyCode != 39) {
             if (isNaN($(event.target).val())) {
                 $(event.target).val("");
             }
 
             var inputValues = ["0"];
-            if($(event.target).attr("id") != "num1") inputValues.push($("#num1").val());
-            if($(event.target).attr("id") != "num2") inputValues.push($("#num2").val());
-            if($(event.target).attr("id") != "num3") inputValues.push($("#num3").val());
-            if($(event.target).attr("id") != "num4") inputValues.push($("#num4").val());
+            if ($(event.target).attr("id") != "num1") inputValues.push($("#num1").val());
+            if ($(event.target).attr("id") != "num2") inputValues.push($("#num2").val());
+            if ($(event.target).attr("id") != "num3") inputValues.push($("#num3").val());
+            if ($(event.target).attr("id") != "num4") inputValues.push($("#num4").val());
 
-            if(inputValues.indexOf($(event.target).val()) != -1) {
+            if (inputValues.indexOf($(event.target).val()) != -1) {
                 $(event.target).val("");
             }
 
@@ -88,8 +94,8 @@ class Game {
         $("#num4").val("");
     }
 
-    checkWin(result : resultJson) {
-        if(result.right >= 4) {
+    checkWin(result: resultJson) {
+        if (result.right >= 4) {
             $(".icon").removeClass("icon-lock-closed").addClass("icon-lock-open");
 
             return true;
